c7bead899e3500683ead6c33561d616022491f2a
Mikkel Ricky <rimi@aarhus.dk>
2022-10-02 08:58:23 +0200

    DW-277: Added some MeMo message validation

--- src/Service/SF1601/SF1601.php
+++ src/Service/SF1601/SF1601.php
@@ -13,11 +13,15 @@ namespace ItkDev\Serviceplatformen\Service\SF1601;
 use DateTimeInterface;
 use DigitalPost\MeMo\Message;
 use DOMDocument;
+use DOMElement;
 use DOMText;
+use DOMXPath;
 use ItkDev\Serviceplatformen\Service\AbstractRESTService;
 use ItkDev\Serviceplatformen\Service\Exception\ServiceException;
+use ItkDev\Serviceplatformen\Service\SF1601\Exception\InvalidMemoException;
 use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\HttpClient\ResponseInterface;
 
 class SF1601 extends AbstractRESTService
 {
@@ -25,6 +29,8 @@ class SF1601 extends AbstractRESTService
     public const TYPE_FYSISK_POST = 'Fysisk Post';
     public const TYPE_DIGITAL_POST = 'Digital Post';
     public const TYPE_NEM_SMS = 'NemSMS';
+
+    private ?DOMElement $lastKombiMemoMessage = null;
 
     protected function configureOptions(OptionsResolver $resolver)
     {
@@ -39,9 +45,15 @@ class SF1601 extends AbstractRESTService
         ]);
     }
 
-    public function kombiPostAfsend(string $transactionId, string $type, Message $message, DateTimeInterface $transactionTid = null)
+    public function kombiPostAfsend(string $transactionId, string $type, Message $message, DateTimeInterface $transactionTid = null): ResponseInterface
     {
-        $document = $this->buildKombiRequest($type, $message);
+        $document = $this->buildKombiRequestDocument($type, $message);
+        $xpath = new DOMXPath($document);
+        $xpath->registerNamespace(MemoDocumentNormalizer::MEMO_NAMESPACE_PREFIX, MemoDocumentNormalizer::MEMO_NAMESPACE_URI);
+        foreach ($xpath->query(sprintf('//%s:Message', MemoDocumentNormalizer::MEMO_NAMESPACE_PREFIX)) as $element) {
+            $this->lastKombiMemoMessage = $element;
+            break;
+        }
 
         $response = $this->call('POST', $this->getOption('svc_endpoint'), [
                 'headers' => [
@@ -56,7 +68,15 @@ class SF1601 extends AbstractRESTService
         return $response;
     }
 
-    private function buildKombiRequest(string $type, Message $message)
+    /**
+     * @return DOMElement|null
+     */
+    public function getLastKombiMeMoMessage(): ?DOMElement
+    {
+        return $this->lastKombiMemoMessage;
+    }
+
+    private function buildKombiRequestDocument(string $type, Message $message): DOMDocument
     {
         if (!in_array($type, [self::TYPE_AUTOMATISK_VALG, self::TYPE_DIGITAL_POST, self::TYPE_FYSISK_POST, self::TYPE_NEM_SMS])) {
             throw new ServiceException(sprintf('Invalid type: %s', $type));
@@ -67,6 +87,29 @@ class SF1601 extends AbstractRESTService
         $document->loadXML('<kombi_request><KombiValgKode/></kombi_request>');
         // Set KombiValgKode.
         $document->documentElement->firstChild->appendChild(new DOMText($type));
+
+        // Set default values on some required attributes.
+        if (empty($message->getMemoVersion())) {
+            $message->setMemoVersion(1.1);
+        }
+        if (empty($message->getMemoSchVersion())) {
+            $message->setMemoSchVersion('1.1.0');
+        }
+
+        // Validate message
+        $header = $message->getMessageHeader();
+        // We ignore some PHPStan errors due to generated code not having correct return type annotations.
+        // @phpstan-ignore-next-line
+        if (empty($header)) {
+            throw new InvalidMemoException('MeMo message must have a header');
+        }
+        if (empty($header->getLabel())) {
+            throw new InvalidMemoException('MeMo message header must have a label');
+        }
+        // @phpstan-ignore-next-line
+        if (empty($header->getSender()) || empty($header->getSender()->getLabel())) {
+            throw new InvalidMemoException('MeMo message header must have a sender with a label');
+        }
 
         // Serialize message and import and append it to kombi_request element.
         $messageDocument = new DOMDocument();
