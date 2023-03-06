b9bae6d2eaf4c623581cb7ee1d6e8f29139e4746
Mikkel Ricky <rimi@aarhus.dk>
2023-03-06 15:58:57 +0100

    FORMS-674: Cleaned up and added search command

--- src/Command/SF1500/SF1500LookupCommand.php
+++ src/Command/SF1500/SF1500LookupCommand.php
@@ -10,48 +10,31 @@
 
 namespace ItkDev\Serviceplatformen\Command\SF1500;
 
-use GuzzleHttp\Client;
-use Http\Adapter\Guzzle6\Client as GuzzleAdapter;
-use Http\Factory\Guzzle\RequestFactory;
-use ItkDev\AzureKeyVault\Authorisation\VaultToken;
-use ItkDev\AzureKeyVault\KeyVault\VaultSecret;
-use ItkDev\Serviceplatformen\Certificate\AzureKeyVaultCertificateLocator;
-use ItkDev\Serviceplatformen\Certificate\CertificateLocatorInterface;
-use ItkDev\Serviceplatformen\Certificate\FilesystemCertificateLocator;
 use ItkDev\Serviceplatformen\Service\Exception\InvalidArgumentException;
 use ItkDev\Serviceplatformen\Service\Exception\SF1500Exception;
 use ItkDev\Serviceplatformen\Service\SF1500\SF1500;
-use ItkDev\Serviceplatformen\Service\SF1500\SF1500XMLBuilder;
-use ItkDev\Serviceplatformen\Service\SF1514\SF1514;
-use ItkDev\Serviceplatformen\Service\SoapClient;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Exception\InvalidOptionException;
 use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\PropertyAccess\PropertyAccessor;
 
-class SF1500LookupCommand extends Command
+class SF1500LookupCommand extends AbstractSF1500Command
 {
-    const SF1500_SERVICE_ENDPOINT = 'http://stoettesystemerne.dk/service/organisation/3';
-
     protected static $defaultName = 'serviceplatformen:sf1500:lookup';
-    private array $inputOptions = [];
 
     protected function configure()
     {
-        $inputOptions = [
+        $definition = [
             new InputOption('manager', null, InputOption::VALUE_NONE, 'get information on manager for provided user-id'),
             new InputOption('production', null, InputOption::VALUE_NONE, 'production mode'),
             new InputOption('user-id', null, InputOption::VALUE_REQUIRED, 'user id'),
             new InputOption('certificate', null, InputOption::VALUE_REQUIRED, 'path to certificate'),
-            new InputOption('passphrase', null, InputOption::VALUE_OPTIONAL, 'passphrase for certificate', ''),
+            new InputOption('certificate-passphrase', null, InputOption::VALUE_OPTIONAL, 'passphrase for certificate', ''),
             new InputOption('authority-cvr', null, InputOption::VALUE_REQUIRED, 'authority cvr'),
             new InputOption('manager-type-id', null, InputOption::VALUE_REQUIRED, 'manager type id'),
         ];
-        $this->setDefinition(new InputDefinition($inputOptions));
+        $this->setDefinition(new InputDefinition($definition));
 
         $this->setDescription('Helper script for testing “SF1500: Organisation”.');
 
@@ -78,7 +61,7 @@ certificate:
     If using path option make sure the path is relative to project root to the certificate file, e.g.
     '/app/src/Command/SF1500/certificate.p12'
  
-passphrase:
+certificate-passphrase:
     the passphrase for p12 certificate, i.e.
     'XYZ'
     defaults to the empty string if not provided
@@ -97,8 +80,7 @@ HELP;
 
         $this->setHelp($help);
 
-        // Index by option name.
-        $this->inputOptions = array_combine(array_map(fn (InputOption $option) => $option->getName(), $inputOptions), $inputOptions);
+        $this->buildInputOptions($definition);
     }
 
     /**
@@ -106,36 +88,13 @@ HELP;
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $resolver = new OptionsResolver();
-        $this->configureOptions($resolver);
-        $options = $resolver->resolve(
+        $options = $this->resolveOptions(
             array_filter(array_filter($input->getOptions()), fn ($name) => isset($this->inputOptions[$name]), ARRAY_FILTER_USE_KEY)
         );
 
         $userId = $options['user-id'];
 
-        $propertyAccessor = new PropertyAccessor();
-
-        $certificateLocator = $this->getCertificateLocator($options['certificate'], $options['passphrase']);
-
-        $soapClient = new SoapClient([
-            'cache_expiration_time' => ['7am', 'tomorrow 7am'],
-        ]);
-
-        $serviceOptions = [
-            'certificate_locator' => $certificateLocator,
-            'authority_cvr' => $options['authority-cvr'],
-            'sts_applies_to' => self::SF1500_SERVICE_ENDPOINT,
-            'test_mode' => !$options['production'],
-        ];
-
-        $sf1514 = new SF1514($soapClient, $serviceOptions);
-
-        $sf1500XMLBuilder = new SF1500XMLBuilder();
-
-        unset($serviceOptions['sts_applies_to']);
-
-        $sf1500 = new SF1500($soapClient, $sf1514, $sf1500XMLBuilder, $propertyAccessor, $serviceOptions);
+        $sf1500 = $this->getSF1500($options);
 
         // Use a lookup on name as an indicator for whether user-id exists or not.
         $name = $sf1500->getPersonName($userId);
@@ -211,82 +170,14 @@ HELP;
         $output->writeln('Magistrat: '. $sf1500->getPersonMagistrat($funktionsId));
     }
 
-    private function getCertificateLocator(string $spec, string $passphrase): CertificateLocatorInterface
+    protected function configureOptions(OptionsResolver $resolver): OptionsResolver
     {
-        if (false !== strpos($spec, '=')) {
-            parse_str($spec, $options);
-            $resolver = $this->getCertificateOptionsResolver();
-            $options = $resolver->resolve($options);
-
-            $httpClient = new GuzzleAdapter(new Client());
-            $requestFactory = new RequestFactory();
-
-            $vaultToken = new VaultToken($httpClient, $requestFactory);
-
-            $token = $vaultToken->getToken(
-                $options['tenant-id'],
-                $options['client-id'],
-                $options['client-secret'],
-            );
-
-            $vault = new VaultSecret(
-                $httpClient,
-                $requestFactory,
-                $options['name'],
-                $token->getAccessToken()
-            );
-
-            return new AzureKeyVaultCertificateLocator(
-                $vault,
-                $options['secret'],
-                $options['version'],
-                $passphrase
-            );
-        } else {
-            $certificatepath = realpath($spec) ?: null;
-            if (null === $certificatepath) {
-                throw new InvalidOptionException(sprintf('Invalid path %s', $spec));
-            }
-            return new FilesystemCertificateLocator($certificatepath, $passphrase);
-        }
-    }
-
-    private function getCertificateOptionsResolver(): OptionsResolver
-    {
-        $resolver = new OptionsResolver();
-        $resolver
-            ->setRequired([
-                'tenant-id',
-                'client-id',
-                'client-secret',
-                'name',
-                'secret',
-                'version',
-            ])
-            ->setInfo('tenant-id', 'The tenant id')
-            ->setInfo('client-id', 'The client id')
-            ->setInfo('client-secret', 'The client secret')
-            ->setInfo('name', 'The certificate name')
-            ->setInfo('secret', 'The certificate secret')
-            ->setInfo('version', 'The certificate version')
-        ;
-
-        return $resolver;
-    }
-
-    private function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver
+        return parent::configureOptions($resolver)
             ->setRequired([
                 'user-id',
-                'certificate',
-                'passphrase',
-                'authority-cvr',
                 'manager-type-id',
             ])
             ->setDefaults([
-                'passphrase' => '',
-                'production' => false,
                 'manager' => false,
             ])
         ;
